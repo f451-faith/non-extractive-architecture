4b7ab0142888a0ceccc300db667ccf26e22def16
Quentin Creuzet <quentin@f451.faith>
2021-02-09 12:41:37 +0100

    Setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,27 +1,33 @@
+const routerBase =
+  process.env.DEPLOY_ENV === 'GH_PAGES'
+    ? {
+      router: {
+        base: '/non-extractive-architecture/'
+      }
+    }
+    : {}
+
 export default {
+  ...routerBase,
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'non-extractive-architecture',
+    title: 'Non-Extractive Architecture',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'stylesheet', href: 'https://use.typekit.net/ism7vts.css' }]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+  css: ['~/assets/scss/application'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -29,14 +35,24 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/style-resources'
   ],
 
+  styleResources: {
+    scss: ['~assets/scss/*/*.scss']
+  },
+
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-  ],
+  modules: [],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+    extend (config) {
+      config.module.rules.push({
+        test: /\.pdf$/,
+        loader: 'file-loader'
+      })
+    }
   }
 }
